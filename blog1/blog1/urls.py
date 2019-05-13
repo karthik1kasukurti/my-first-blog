5ca23d690a156780fb7a07be45a6630a9687d46a
karthikkasukurti <karthik.kasukurti@gmail.com>
2019-05-13 22:12:51 +0530

    changed html for this site.

--- blog1/blog1/urls.py
+++ blog1/blog1/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+from .views import (home_page,about_page)
 
 urlpatterns = [
+	#path('page/',about_page),
+	#path('pages/',about_page),
+	re_path(r'^pages?/$',about_page),	#this is same as combination of above two lines
+	path('', home_page),
+	path('home/',home_page),
     path('admin/', admin.site.urls),
 ]
